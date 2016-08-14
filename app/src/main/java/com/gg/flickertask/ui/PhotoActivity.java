63320d7473db917531a733652965a667d3291b0a
gilad gotman <gilad.gotman@altran.com>
2016-08-14 15:17:58 +0200

    add butterKnife, add information in photoActivity

--- app/src/main/java/com/gg/flickertask/ui/PhotoActivity.java
+++ app/src/main/java/com/gg/flickertask/ui/PhotoActivity.java
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gg.flickertask.R;
 import com.gg.flickertask.model.Photo;
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -20,25 +22,34 @@ public class PhotoActivity extends AppCompatActivity {
     
     private static final String TAG = PhotoActivity.class.getSimpleName();
     ImageView mFullScreenPhoto;
+    @BindView(R.id.owener_name_value_txv)
+    TextView mOwnerNameTxv;
+    @BindView(R.id.date_value_txv)
+    TextView mDateTxv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ButterKnife.bind(this);
         setContentView(R.layout.activity_photo);
+        ButterKnife.setDebug(true);
+        ButterKnife.bind(PhotoActivity.this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         mFullScreenPhoto = (ImageView) findViewById(R.id.fullscreenPhoto);
         Intent photoIntent = getIntent();
         if (photoIntent != null) {
             Photo photo = (Photo) photoIntent.getSerializableExtra(MainActivity.PHOTO_OBJECT_KEY);
+            toolbar.setTitle(photo.getTitle());
             String photoUrl = photo.getUrlOriginalSize();
-            Log.d(TAG, "onCreate: photo title:" + photo.getTitle());
             if (photoUrl != null) {
                 Picasso.with(PhotoActivity.this)
                         .load(photoUrl)
                         .into(mFullScreenPhoto);
             }
+            mOwnerNameTxv.setText(photo.getOwenerName());
+            mDateTxv.setText(photo.getDateTaken());
+            Log.d(TAG, "onCreate: photo title:" + photo.getTitle());
+
         }
+        setSupportActionBar(toolbar);
     }
 }
